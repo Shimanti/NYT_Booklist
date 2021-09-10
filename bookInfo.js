c240dd11ef8d49e9ba58928ab26307c764e257a9
Shimanti <shimanti@gmail.com>
2021-09-10 12:55:53 -0700

    added Amazon link

---
+++ bookInfo.js
@@ -0,0 +1,3 @@
+API_Key='wuDGORmXAWnBvUGYjc30Wsqgtz87l1wj'
+URL = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_Key}`
+
--- script.js
+++ script.js
@@ -2,15 +2,17 @@ API_Key='wuDGORmXAWnBvUGYjc30Wsqgtz87l1wj'
 URL = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_Key}`
 
 const bookList = document.getElementById("book-list")
-getInfo()
+getData()
 
-async function getInfo() {
+async function getData() {
     const res = await fetch(URL)
     const data = await res.json()
     // console.log(data.results.books)
     const books = data.results.books
     books.forEach((book,index) => {
-        console.log(book)
+        // console.log(book)
+        const amazonLink = book.amazon_product_url
+        const shimLink = amazonLink.replace("NYTBSREV-20","shimanti-20")
         const li = document.createElement('li')
         li.innerHTML = `
             <span class="number">${index + 1}</span>
@@ -18,7 +20,13 @@ async function getInfo() {
                 <p class="book-name">${book.title}</p>
                 <p class="book-author">by ${book.author}</p>
             </div>
+            <a class="book-url" href="${shimLink}">
+                <i class="fas fa-link"></i>
+             </a>
+        
         `
         bookList.appendChild(li)
     })
 }
+
+
