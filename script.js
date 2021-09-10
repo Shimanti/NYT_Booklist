9248aa28e9bcb79482f13d85b4c2c3b603df4a6a
Shimanti <shimanti@gmail.com>
2021-09-09 23:01:30 -0700

    added async/await

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
-const book_list = document.getElementById("book-list")
+API_Key='wuDGORmXAWnBvUGYjc30Wsqgtz87l1wj'
+URL = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_Key}`
 
-const fiction = [
-"The Madness of Crowds by Louise Penny",
-"Billy Summers by Stephen King",
-"The Last Thing He Told Me by Laura Dave",
-"The Love Songs of W. E. B. du Bois by Honorée Fanonne Jeffers",
-"Lightning Strike by William Kent Krueger",
-"The Midnight Library by Matt Haig",
-"The Noise by James Patterson; J. D. Barker",
-"Complications by Danielle Steel",
-"The Paper Palace by Miranda Cowley Heller",
-"Bloodless by Douglas Preston; Lincoln Child",
-"Malibu Rising by Taylor Jenkins Reid",
-"The President's Daughter by James Patterson; Bill Clinton",
-"The Invisible Life of Addie Larue by V. E. Schwab",
-"We Were Never Here by Andrea Bartz",
-"Project Hail Mary by Andy Weir"
-]
+const bookList = document.getElementById("book-list")
+getInfo()
 
-const listItems = []
-listBooks()
-
-function listBooks() {
-  fiction.forEach((book,index) => {
-      const listItem = document.createElement('li')
-      listItem.setAttribute('data-index', index)
-      listItem.innerHTML = `
-        <span class="number">${index + 1}</span>
-        <div class="book">
-          <p class="book-name">${book}</p>
-        </div>
-      `
-      listItems.push(listItem)
-      book_list.appendChild(listItem)
-  })
+async function getInfo() {
+    const res = await fetch(URL)
+    const data = await res.json()
+    // console.log(data.results.books)
+    const books = data.results.books
+    books.forEach((book,index) => {
+        console.log(book)
+        const li = document.createElement('li')
+        li.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="book">
+                <p class="book-name">${book.title}</p>
+                <p class="book-author">by ${book.author}</p>
+            </div>
+        `
+        bookList.appendChild(li)
+    })
 }
